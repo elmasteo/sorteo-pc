a9ad3128efefd8466883efc26a5414de6ccad4df
unknown <sago980302@hotmail.com>
2025-07-01 17:26:07 -0500

    ajuste reservar

--- netlify/functions/reservar.js
+++ netlify/functions/reservar.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 exports.handler = async (event) => {
   const { numero, nombre, telefono } = JSON.parse(event.body);
-  const file = path.join(__dirname, '../../data/boletas.json');
+  const file = path.join(__dirname, '../../boletas.json');
   const data = JSON.parse(fs.readFileSync(file));
 
   const boleta = data.find(b => b.numero === numero);
